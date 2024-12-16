d949a42fa515429c1b811e2df3c5fd3044142ad9
Patrik Lažek <patalazek@seznam.cz>
2024-12-16 02:12:09 +0100

    Before anim

--- data/exe5/IZHV_E5/Assets/Scripts/Player/Character2DMovement.cs
+++ data/exe5/IZHV_E5/Assets/Scripts/Player/Character2DMovement.cs
@@ -212,6 +212,17 @@ public class Character2DMovement : MonoBehaviour
 	     *   * Persistent heading flag: *mHeadingRight*
 	     *   * Rotating a local rotation by an axis: localRotation *= Quaternion.Euler(...)
 	     */
+        
+        if (mInput.move.x > 0 && !mHeadingRight)
+        {
+            mHeadingRight = true;
+            transform.localRotation *= Quaternion.Euler(0, 180, 0);
+        }
+        else if (mInput.move.x < 0 && mHeadingRight)
+        {
+            mHeadingRight = false;
+            transform.localRotation *= Quaternion.Euler(0, 180, 0);
+        }
 	    
 	    var animator = mSelector.charAnimator;
 	    if (animator != null)
@@ -265,6 +276,13 @@ public class Character2DMovement : MonoBehaviour
 			 *   * Current Animator instance: *animator*
 			 *   * Animator methods: *SetFloat* and *SetBool*
 			 */
+
+            animator.SetFloat("Speed", speed);
+            animator.SetFloat("MoveSpeed", moveSpeed);
+            animator.SetBool("Crouch", crouch);
+            animator.SetBool("Grounded", grounded);
+            animator.SetBool("Jump", jump);
+            animator.SetBool("Fall", falling);
 	    }
     }
 }
